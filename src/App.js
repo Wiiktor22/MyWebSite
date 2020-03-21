ea08c0c1475964da3d7c1f4390a0ebabad5717ec
Wiktor Szlegier <22wiktors@gmail.com>
2020-03-21 13:29:35 +0100

    Prepare project to work

--- src/App.js
+++ src/App.js
@@ -1,26 +1,42 @@
 import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import styled, { createGlobalStyle } from 'styled-components';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+const GlobalStyle = createGlobalStyle`
+    @import url('https://fonts.googleapis.com/css2?family=Montserrat:wght@300;400&family=Raleway:wght@300;400&display=swap');
+    
+    * {
+        margin: 0;
+        padding: 0;
+        box-sizing: border-box;
+    }
+
+    body {
+        font-size: 10px;
+        font-family: 'Montserrat', sans-serif;
+        font-weight: 300;
+        color: white;
+    }
+`;
+
+const Wrapper = styled.div`
+    height: 100vh;
+    width: 100%;
+    background: rgb(28,43,54);
+    background: -moz-linear-gradient(180deg, rgba(28,43,54,1) 0%, rgba(66,90,128,1) 80%);
+    background: -webkit-linear-gradient(180deg, rgba(28,43,54,1) 0%, rgba(66,90,128,1) 80%);
+    background: linear-gradient(180deg, rgba(28,43,54,1) 0%, rgba(66,90,128,1) 80%);
+    filter: progid:DXImageTransform.Microsoft.gradient(startColorstr="#1c2b36",endColorstr="#425a80",GradientType=1);
+`;
+
+const App = () => {
+    return (
+        <>
+            <GlobalStyle/>
+            <Wrapper>
+                <h1>Siema</h1>
+            </Wrapper>
+        </>
+    );
 }
-
+ 
 export default App;
